d86b4016a74f435aac8758d9a04166f30f7ede1b
David Brodie <david.brodie122@gmail.com>
2019-09-23 11:57:32 -0400

    Table Definition for TechnologyTimePeriod plus initial db proj setup

---
+++ JohnBlackburn/src/Tables/TechnologyTimePeriod.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TechnologyTimePeriod]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [TechnologyName] varchar(200) NOT NULL, 
+    [StartDate] DATE NOT NULL, 
+    [EndDate] DATE NULL, 
+    [JobTitle] VARCHAR(200) NOT NULL, 
+    [MajorProjectName] VARCHAR(200) NOT NULL, 
+    [MajorProjectAlias] VARCHAR(200) NOT NULL,
+	[UpdateDT] DateTime NULL DEFAULT GETUTCDATE(),
+
+)
