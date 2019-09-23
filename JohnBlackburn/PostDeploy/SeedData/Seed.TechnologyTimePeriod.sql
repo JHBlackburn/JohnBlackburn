cfd1fd4d5cd51d655e162cf689b2b30adc7aec2c
David Brodie <david.brodie122@gmail.com>
2019-09-23 12:17:04 -0400

    WIP, Does not build . Working on Post Deploy seed data.

---
+++ JohnBlackburn/PostDeploy/SeedData/Seed.TechnologyTimePeriod.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO [dbo].[TechnologyTimePeriod]
+		(
+			[TechnologyName], 
+			[StartDate], 
+			[EndDate] , 
+			[JobTitle] , 
+			[MajorProjectPrivateName] , 
+			[MajorProjectPublicName] ,
+			[UpdateDT]
+		)
+VALUES (
+			('T-SQL', '20150101', null, 'Business Systems Analyst I', 'Bid Database 2.0', 'CRM #1', GETUTCDATE())
+
+		);
--- JohnBlackburn/src/Tables/TechnologyTimePeriod.sql
+++ JohnBlackburn/src/Tables/TechnologyTimePeriod.sql
@@ -2,11 +2,11 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [TechnologyName] varchar(200) NOT NULL, 
-    [StartDate] DATE NOT NULL, 
+    [StartDate] DATE NOT NULL DEFAULT GETUTCDATE(), 
     [EndDate] DATE NULL, 
     [JobTitle] VARCHAR(200) NOT NULL, 
-    [MajorProjectName] VARCHAR(200) NOT NULL, 
-    [MajorProjectAlias] VARCHAR(200) NOT NULL,
+    [MajorProjectPrivateName] VARCHAR(200) NOT NULL, 
+    [MajorProjectPublicName] VARCHAR(200) NOT NULL,
 	[UpdateDT] DateTime NULL DEFAULT GETUTCDATE(),
 
 )
